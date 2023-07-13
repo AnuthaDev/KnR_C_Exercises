3c2457eac6f95aa5ff7d39d791f0616e13e93edb
Anurag Thakur <anuthadev@gmail.com>
2023-07-13 19:22:14 +0530

    Added Exercise 1-2

---
+++ ch1/ex1-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	printf("Namaste!\n");
+}
+
+/*
+ Replacing \n with:
+ \a - (Bell Sound)
+ \b - "Namaste"
+ \c - warning: unknown escape sequence: '\c'
+ \\ - "Namaste\"
+ \0 - "Namaste!"
+*/
